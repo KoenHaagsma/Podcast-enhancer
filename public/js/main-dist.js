e27945cce464040eb33eee28ffa09eae9f7239e6
Koen Haagsma <koenhaagsma123@hotmail.nl>
2022-04-21 17:58:57 +0200

    Setup text

--- public/js/main-dist.js
+++ public/js/main-dist.js
@@ -1,1 +1,1 @@
-"use strict";console.log("test");
+"use strict";var html=document.querySelector("html");function switchTheme(t){html.dataset.theme="theme-".concat(t)}html.dataset.theme="theme-light";
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,6 @@
-console.log('test');
+const html = document.querySelector('html');
+html.dataset.theme = `theme-light`;
+
+function switchTheme(theme) {
+    html.dataset.theme = `theme-${theme}`;
+}
